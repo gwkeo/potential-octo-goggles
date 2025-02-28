414d79a5d97ecf7781a6f39a14fbd998dfdc1f01
Rakhman Imanov <raxman.i@mail.ru>
2025-02-28 19:35:58 +0300

    Добавлен тестовый функционал с элементами telegram mini app. Убраны ненужные объявления типов

--- pog-tma/src/App.tsx
+++ pog-tma/src/App.tsx
@@ -1,13 +1,8 @@
-import { Component } from 'solid-js';
+import { Component, onMount } from 'solid-js';
 import { createSignal } from 'solid-js';
 import { Task } from './components/task/task';
 
 const App: Component = () => {
-  const [input, setInput] = createSignal("")
-
-  let clicked = () => {
-    console.log(JSON.stringify({input: input()}))
-  }
 
   return (
     <div>
--- pog-tma/src/components/task/task.tsx
+++ pog-tma/src/components/task/task.tsx
@@ -1,9 +1,21 @@
 import { renderToString } from "katex";
-import { Component, createSignal, For } from "solid-js";
+import { Component, createSignal, For, onMount } from "solid-js";
 
 import "./task.css"
 
 export const Task : Component = () => {
+    onMount(() => {
+        if (window?.Telegram?.WebApp) {
+            let app = window.Telegram.WebApp
+            app.MainButton
+                .setText("Отправить")
+                .onClick(handleSubmit)
+                .show()
+            
+            app.BackButton.onClick(() => app.showAlert("back button pressed"))
+        }
+    })
+
     const fields = ['name', 
         'formula', 
         'focus1', 
@@ -37,6 +49,7 @@ export const Task : Component = () => {
             <For each={[...fields]}>
                 {(element, i) => 
                 <div class="content">
+                    <div>{element}</div>
                     <input placeholder="Пример, x^2" type="text" onInput={(e) => handleInput(element, (e.currentTarget as HTMLInputElement).value)}/>
                     <div class="tex" innerHTML={handleTex(element)}></div>
                 </div>
@@ -45,6 +58,7 @@ export const Task : Component = () => {
                 <button class="cancel" onclick={() => {console.log("return back")}}>back</button>
                 <button class="submit" onclick={() => {handleSubmit()}}>Submit</button>
             </div>
+
         </div>
     )
 }
