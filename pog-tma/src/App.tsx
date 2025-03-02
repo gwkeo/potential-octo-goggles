df4449078b92caea01b8b29f3410ef23198e1054
Rakhman Imanov <raxman.i@mail.ru>
2025-03-03 02:07:05 +0300

    Добавлена навигационная панель со вкладками профиля и теста, в профиле отображается id чата пользователя

--- pog-tma/src/App.tsx
+++ pog-tma/src/App.tsx
@@ -1,12 +1,26 @@
-import { Component, onMount } from 'solid-js';
-import { createSignal } from 'solid-js';
+import { Component, createSignal } from 'solid-js';
+import { Dynamic } from 'solid-js/web';
 import { Task } from './components/task/task';
+import { Profile } from './components/profile/profile';
+
+import "./App.module.css"
 
 const App: Component = () => {
 
+  const sections = {
+    "profile": Profile,
+    "task": Task
+  }
+
+  const [section, setSection] = createSignal<string>("task")
+
   return (
-    <div>
-      <Task/>
+    <div class="main">
+      <nav>
+          <button onclick={() => setSection("profile")}>Профиль</button>
+          <button onclick={() => setSection("task")}>Задачи</button>
+      </nav>
+      <Dynamic component={sections[section()]}></Dynamic>
     </div>
   );
 };
