b2a3f66c64e34886fbb1e3b43a7751396248f1f5
Rakhman Imanov <raxman.i@mail.ru>
2025-03-08 00:01:36 +0300

    Дополнена миграция с assignments

--- migrations/20250223114359_assignment.sql
+++ migrations/20250223114359_assignment.sql
@@ -1,9 +1,15 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE assignments (
+CREATE TABLE IF NOT EXISTS assignments (
     id SERIAL PRIMARY KEY,
     user_id INTEGER,
     formula VARCHAR(256),
     grade FLOAT
 );
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+
+DROP TABLE assignments;
 -- +goose StatementEnd
